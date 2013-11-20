bdaa3bec4907b9e8d5f7238b919d8e619b4be3f1
aragua <fabienlahoudere@gmail.com>
2013-11-20 13:42:30 +0100

    Create ledctl.c

---
+++ examples/meta-insa/recipes-drivers/led/led/ledctl.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main( int argc, char ** argv)
+{
+	int fd = open( "/dev/led", O_RDWR );
+	if (fd < 0 )
+	{
+		printf ( "Fail to open %s \n", strerror(errno));
+		return  -1;
+	}
+
+	while (1)
+	{
+		sleep(1);
+	}
+
+	close(fd);
+
+	return 0;
+}
